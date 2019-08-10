ce66e966b523f39c37c38ba3fd8e390cabb42213
Henry Blevins <heblevi@gmail.com>
2019-08-10 16:36:18 -0400

    Vue Todo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,101 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Todos</h1>
+    <div>
+      <div class="todo" v-bind:class="{ done: todo.completed }" v-for="todo in todos" v-on:click="toggle(todo.id)" :key="todo.id">
+        {{ todo.task }}
+      </div>
+      <form v-on:submit="addTodo">
+        <input v-model="newTask" placeholder="add todo"/>
+        <button type="sumbit">add todo</button>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+ export default {
+     name: 'app',
+     data() {
+         return {
+             newTask: "",
+             todos: [
+                 {
+                     id: 1,
+                     task: "Learn vue",
+                     completed: false
+                 },
+                 {
+                     id: 2,
+                     task: "Eat food",
+                     completed: true
+                 }
+             ]
+         }
+     },
+     methods: {
+         toggle: function(id) {
+             this.todos = this.todos.map(todo => {
+                 if (todo.id === id) {
+                     return {...todo, completed: !todo.completed}
+                 }
+                 return todo;
+             });
+         },
+         addTodo: function(event) {
+             event.preventDefault();
+             this.todos.push({
+                 id: Date.now(),
+                 task: this.newTask,
+                 completed: false
+             })
+         }
+     }
+ }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+ #app {
+     font-family: 'Avenir', Helvetica, Arial, sans-serif;
+     -webkit-font-smoothing: antialiased;
+     -moz-osx-font-smoothing: grayscale;
+     text-align: center;
+     color: #2c3e50;
+     margin-top: 60px;
+ }
+ .todo {
+     padding: 10px;
+     margin: 10px auto;
+     background-color: #fafafa;
+     /* rgb rrggbb
+        00-99
+        909999
+        000000
+        990000
+        0 1 2 3 4 5 6 7 8 9
+        0 1 2 3 4 5 6 7 8 9 a b c d e f
+      */
+     border: 1px solid #1a1a1a;
+     border-radius: 5px;
+     max-width: 500px;
+     text-align: left;
+     transition: background-color 0.3s;
+     cursor: pointer;
+ }
+
+ .todo:hover {
+     background-color: #dadada;
+ }
+
+ .todo.done {
+ }
+
+ .todo.done::before {
+     content: "✓";
+     background-color: #74AC7A;
+     border-radius: 100%;
+     padding: 3px;
+     color: white;
+     text-weight: bold;
+ }
 </style>
